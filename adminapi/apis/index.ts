88175931b3c2842a543f0b6b7ba8b41c0bfa33ee
willtpa <willys.tjiawi@triple-a.io>
2024-06-26 11:48:47 +0800

    feat: adminapi

---
+++ adminapi/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountApi';
+export * from './EntityApi';
+export * from './RatesApi';
---
+++ adminapi/models/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountDetails';
+export * from './Corporate';
+export * from './CountryCode2';
+export * from './CreateEntityCorporate';
+export * from './CreateEntityIndividual';
+export * from './Entity';
+export * from './Gender';
+export * from './Individual';
+export * from './MedianFxRateV1';
+export * from './PostAdminApiV1EntitiesRequest';
+export * from './ProblemDetail';
+export * from './Transaction';
